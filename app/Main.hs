2c7d614e8465189a23722b3762c820a1babad962
Alena <al.venedictova@yandex.ru>
2022-08-01 15:12:30 +0300

    Added support of repeat command

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,4 @@ main = do
   case mbConfig of
     Nothing -> putStrLn "Couldn't parse config"
     Just fromConfig  -> do
-      botLoop 0 fromConfig
-  -- let ti = Times 1
-  -- saying ti 0
-  --   where
-  --   saying :: Times -> Int -> IO ()
-  --   saying t offset = do
-  --       chatid <- getChatID
-  --       text <- getUpdateMsg offset chatid
-  --       newti <- helper t offset chatid text
-  --       saying newti (offset + 1)
-  --       pure ()
+      botLoop 0 fromConfig [] []
--- src/Bot.hs
+++ src/Bot.hs
@@ -6,14 +6,21 @@ import Parsing
 import qualified Data.ByteString as B (ByteString, readFile)
 import Data.Aeson
 import Network.HTTP.Simple
-import Network.HTTP.Client.Internal (ResponseTimeout (ResponseTimeoutMicro))
+import Network.HTTP.Client.Internal (ResponseTimeout (ResponseTimeoutMicro), RequestBody (RequestBodyBS))
 import Control.Monad (mzero)
+import qualified Data.ByteString.Char8 as BC
+import Data.Maybe
+import Text.Read (readMaybe)
+
+type RepeatNumbers = [(Int,Int)]
 
 data Config = Config {token :: String
                      ,timeout :: Int
                      ,helpMessage :: String
                      ,repeatMessage :: String
-                     ,repeatNumber :: Int}
+                     ,repeatNumber :: Int
+                     ,repeatAcceptMessage :: String
+                     ,errorMessage :: String}
 
 instance FromJSON Config where
   parseJSON (Object config) = Config <$>
@@ -21,18 +28,20 @@ instance FromJSON Config where
                                 config .: "timeout" <*>
                                 config .: "helpMessage" <*>
                                 config .: "repeatMessage" <*>
-                                config .: "repeatNumber"
+                                config .: "repeatNumber" <*>
+                                config .: "repeatAcceptMessage" <*>
+                                config .: "errorMessage"
   parseJSON _               = mzero
 
-botLoop :: Int -> Config -> IO ()
-botLoop offset config = do
+botLoop :: Int -> Config -> [Int] -> RepeatNumbers -> IO ()
+botLoop offset config chatIdsForRepeat repeatNumbers = do
   telegramResponse <- getUpdates offset config
   let mbUpdates = parseUpdates telegramResponse
   case mbUpdates of
     Nothing -> putStrLn "Couldn't parse telegramResponse"
     Just updates -> do
-      newOffset <- sendMsgs updates config
-      botLoop newOffset config
+      (newOffset, newChatIdsForRepeat, newRepeatNumbers) <- sendMsgs updates config chatIdsForRepeat repeatNumbers
+      botLoop newOffset config newChatIdsForRepeat newRepeatNumbers
 
 getUpdates :: Int -> Config -> IO B.ByteString
 getUpdates offset config = do
@@ -42,43 +51,97 @@ getUpdates offset config = do
 parseUpdates :: B.ByteString -> Maybe TelegramUpdates
 parseUpdates telegramResponse = decodeStrict telegramResponse
 
-sendMsgs :: TelegramUpdates -> Config -> IO Int
-sendMsgs (TelegramUpdates userMessages) config = do
+sendMsgs :: TelegramUpdates -> Config -> [Int] -> RepeatNumbers -> IO (Int,[Int],RepeatNumbers)
+sendMsgs (TelegramUpdates userMessages) config chatIdsForRepeat repeatNumbers = do
   case userMessages of
-    [] -> return 0
+    [] -> return (0,chatIdsForRepeat,repeatNumbers)
     [x] -> case x of
       TextMessage updateId chatId msg -> case msg of
         "/help" -> do
           sendHelpMsg chatId config
-          return  (updateId + 1) 
-        _       -> do
-          sendTextMsg chatId msg config
-          return  (updateId + 1)
+          return  (updateId + 1, chatIdsForRepeat, repeatNumbers)
+        "/repeat" -> do
+          sendRepeatMsg chatId config
+          return  (updateId + 1, chatId:chatIdsForRepeat, repeatNumbers)
+        _       -> if chatId `notElem` chatIdsForRepeat then do
+          sendTextMsg chatId msg config chatIdsForRepeat repeatNumbers
+          return  (updateId + 1, chatIdsForRepeat, repeatNumbers)
+          else do
+            case readMaybe msg :: Maybe Int of
+              Nothing -> do
+                sendErrorMsg chatId config
+                sendRepeatMsg chatId config
+                return (updateId + 1, chatIdsForRepeat, repeatNumbers)
+              Just numb -> do
+                if numb > 0 && numb < 6 then do
+                  sendRepeatAcceptMsg chatId msg config
+                  return (updateId + 1, (filter (/= chatId) chatIdsForRepeat), (chatId,read msg :: Int):repeatNumbers)
+                else do
+                  sendErrorMsg chatId config
+                  sendRepeatMsg chatId config
+                  return (updateId + 1, chatIdsForRepeat, repeatNumbers)
       StickerMessage updateId chatId fileId -> do
-        sendStickerMsg chatId fileId config
-        return  (updateId + 1)
+        sendStickerMsg chatId fileId config repeatNumbers
+        return  (updateId + 1, chatIdsForRepeat, repeatNumbers)
     (x:xs) -> case x of
-      TextMessage updateId chatId msg -> do
-        sendTextMsg chatId msg config
-        sendMsgs (TelegramUpdates xs) config
+      TextMessage updateId chatId msg -> case msg of
+        "/help" -> do
+          sendHelpMsg chatId config
+          sendMsgs (TelegramUpdates xs) config chatIdsForRepeat repeatNumbers
+        "/repeat" -> do
+          sendRepeatMsg chatId config
+          sendMsgs (TelegramUpdates xs) config (chatId:chatIdsForRepeat) repeatNumbers
+        _       -> if chatId `notElem` chatIdsForRepeat then do
+          sendTextMsg chatId msg config chatIdsForRepeat repeatNumbers
+          sendMsgs (TelegramUpdates xs) config chatIdsForRepeat repeatNumbers
+          else do
+            sendRepeatAcceptMsg chatId msg config
+            sendMsgs (TelegramUpdates xs) config (filter (/= chatId) chatIdsForRepeat) ((chatId,read msg :: Int):repeatNumbers)
       StickerMessage updateId chatId fileId -> do
-        sendTextMsg chatId fileId config
-        sendMsgs (TelegramUpdates xs) config
+        sendStickerMsg chatId fileId config repeatNumbers
+        sendMsgs (TelegramUpdates xs) config chatIdsForRepeat repeatNumbers
 
-sendTextMsg :: Int -> String -> Config -> IO ()
-sendTextMsg chatid msg config = do
-    mapM httpNoBody $ replicate (repeatNumber config) $ (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token config ++ "/sendMessage?chat_id=" ++ show chatid ++ "&text=" ++ msg)
+sendTextMsg :: Int -> String -> Config -> [Int] -> RepeatNumbers -> IO ()
+sendTextMsg chatid msg config chatIdsForRepeat repeatNumbers = do
+    mapM httpNoBody $ replicate (if lookup chatid repeatNumbers == Nothing then repeatNumber config else fromJust $ lookup chatid repeatNumbers ) $ (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token config ++ "/sendMessage?chat_id=" ++ show chatid ++ "&text=" ++ msg)
     return ()
 
-sendStickerMsg :: Int -> String -> Config -> IO ()
-sendStickerMsg chatid stickerID config = do
-    mapM httpNoBody $ replicate (repeatNumber config) $ (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token config ++ "/sendSticker?chat_id=" ++ show chatid ++ "&sticker=" ++ stickerID)
+sendStickerMsg :: Int -> String -> Config -> RepeatNumbers -> IO ()
+sendStickerMsg chatid stickerID config repeatNumbers = do
+    mapM httpNoBody $ replicate (if lookup chatid repeatNumbers == Nothing then repeatNumber config else fromJust $ lookup chatid repeatNumbers ) $ (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token config ++ "/sendSticker?chat_id=" ++ show chatid ++ "&sticker=" ++ stickerID)
     return ()
 
 sendHelpMsg :: Int -> Config -> IO ()
 sendHelpMsg chatid config = do
     httpNoBody (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token config ++ "/sendMessage?chat_id=" ++ show chatid ++ "&text=" ++ helpMessage config)
     return ()
+
+sendErrorMsg :: Int -> Config -> IO ()
+sendErrorMsg chatid config = do
+    httpNoBody (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token config ++ "/sendMessage?chat_id=" ++ show chatid ++ "&text=" ++ errorMessage config)
+    return ()
+
+sendRepeatMsg :: Int -> Config -> IO ()
+sendRepeatMsg chatid config = do
+    resp <- httpBS $
+      addRequestHeader "Content-Type" "application/json" $
+      setRequestBody body
+        (setRequestMethod "POST" $
+          parseRequestThrow_ $
+          "https://api.telegram.org/bot" ++ token config ++ "/sendMessage")
+    return ()
+    where body = RequestBodyBS $ BC.pack $ "{\"chat_id\":\"" ++ show chatid ++ "\",\"text\":\"" ++ repeatMessage config ++ "\",\"reply_markup\":{\"keyboard\": [[{\"text\": \"1\"},{\"text\": \"2\"},{\"text\": \"3\"},{\"text\": \"4\"},{\"text\": \"5\"}]]},{\"one_time_keyboard\":\"true\"}}"    
+
+sendRepeatAcceptMsg :: Int -> String -> Config -> IO ()
+sendRepeatAcceptMsg chatid msg config = do
+    resp <- httpBS $
+      addRequestHeader "Content-Type" "application/json" $
+      setRequestBody body
+        (setRequestMethod "POST" $
+          parseRequestThrow_ $
+          "https://api.telegram.org/bot" ++ token config ++ "/sendMessage")
+    return ()
+    where body = RequestBodyBS $ BC.pack $ "{\"chat_id\":\"" ++ show chatid ++ "\",\"text\":\"" ++ repeatAcceptMessage config ++ msg ++ " times\",\"reply_markup\":{\"remove_keyboard\":true}"
 
 getConfig :: IO (Maybe Config)
 getConfig = do
